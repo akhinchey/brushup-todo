505bbb7ad6b45bbe3e5fdb6fdd860cdb1eadebbc
Amanda Hinchey <akhinchey@gmail.com>
2017-04-03 16:38:43 -0400

    Add edit functionality to list title.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,24 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to user_path(@user)
     else
+      @errors = @list.errors.full_messages
       render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to user_path(@user)
+    else
+      @errors = @list.errors.full_messages
+      render "edit"
     end
   end
 
