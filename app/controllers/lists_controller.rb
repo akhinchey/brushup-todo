f1b86723d1a15a9a4e0c095827d396e9814a7f9b
Amanda Hinchey <akhinchey@gmail.com>
2017-04-03 15:48:19 -0400

    Add new list functionality:

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,23 @@ class ListsController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @list = @user.lists.new(list_params)
+    if @list.save
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
   end
 
 end
